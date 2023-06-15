6a8e312fd7f5fbbf1acc89696ca843bd33005af0
Danhschool <116479213+Danhschool@users.noreply.github.com>
2023-06-16 03:57:41 +0700

    Create Iperson.java

---
+++ src/lab3/Iperson.java
@@ -0,0 +1,14 @@
+
+package lab3;
+
+import java.util.List;
+
+
+public interface Iperson {
+    public double f1_c1(List<Person> t);
+    public void f2_c1(List<Person> t);
+    public void f3_c1(List<Person> t);
+    public double f1_c3(List<Person> t);
+    public void f2_c3(List<Person> t);
+    public void f3_c3(List<Person> t);
+}
